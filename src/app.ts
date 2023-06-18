951e01ceba3e16d63f033170d6aa5a380186576c
Gildson Alves <gildson_af@outlook.com>
2023-06-18 15:51:59 -0300

    build: initial config, packages

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify';
+
+const app = fastify({ logger: true });
+
+app.get('/', async (request, reply) => {
+	return { hello: 'Hello World' };
+});
+
+app.listen({ port: 3000 }, (err, address) => {
+	if (err) throw err;
+	console.log('Running at:', address);
+});
