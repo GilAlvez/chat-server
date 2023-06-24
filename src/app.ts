866c6b7d77b61c8b33d98762afc6e4a368d42922
Gildson Alves <gildson_af@outlook.com>
2023-06-24 20:54:00 -0300

    style: update all files w/ new prettier settings

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import fastify from 'fastify';
 const app = fastify({ logger: true });
 
 app.get('/', async (request, reply) => {
-	return { hello: 'Hello World' };
+  return { hello: 'Hello World' };
 });
 
 app.listen({ port: 3000 }, (err, address) => {
-	if (err) throw err;
-	console.log('Running at:', address);
+  if (err) throw err;
+  console.log('Running at:', address);
 });
