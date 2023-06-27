93d5b0e0b115c97611489a35e29ede77b1495538
Gildson Alves <gildson_af@outlook.com>
2023-06-27 08:41:47 -0300

    refact: turns DTO a data interface only

--- src/data/DTO/message_dto.ts
+++ src/data/DTO/message_dto.ts
@@ -1,45 +1,25 @@
-import { IMessage, IUser } from 'domain/entities';
+import { IUser } from 'domain/entities';
 
-export class MessageDTO {
-  constructor(
-    public id: string,
-    public user: IUser,
-    public data: string,
-    public type: MessageType,
-    public status: MessageStatus,
-    public userId: string,
-    public roomId: string,
-    public metadata?: string,
-    public createdAt?: Date,
-    public updatedAt?: Date,
-  ) {}
+export interface InputMessageDTO {
+  id: string;
+  user: IUser;
+  data: unknown;
+  metadata?: unknown;
+  type: MessageType;
+  status: MessageStatus;
+  roomId: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
-  static fromEntity(message: IMessage): MessageDTO {
-    return new MessageDTO(
-      message.id,
-      message.user,
-      JSON.stringify(message.data),
-      message.type,
-      message.status,
-      message.user.id,
-      message.roomId,
-      JSON.stringify(message.metadata),
-      message.createdAt,
-      message.updatedAt,
-    );
-  }
-
-  public toEntity(): IMessage {
-    return {
-      id: this.id,
-      type: this.type,
-      status: this.status,
-      data: JSON.parse(this.data),
-      metadata: this.metadata && JSON.parse(this.metadata),
-      user: this.user,
-      roomId: this.roomId,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt,
-    };
-  }
+export interface OutputMessageDTO {
+  id: string;
+  userId: string;
+  data: string;
+  metadata?: string;
+  type: MessageType;
+  status: MessageStatus;
+  roomId: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
